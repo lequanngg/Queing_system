cb87b142a36105ce0b4f42d96f569a2ca68c7fb3
hiep <hiep>
2023-03-15 18:35:13 +0700

    save code

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,20 @@ import Login from "./features/login";
 import Home from "./components/";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./features/homePage/HomePage";
-import Task from "./features/task/Task";
-import Project from "./features/project/Project";
 import Chat from "./features/chat/Chat";
-import AuthProvider from "./Context/AuthProvider";
-import AppProvider from "./Context/AppProvider";
-import LoginChat from "./components/componentsChat/Login";
-import ChatRoom from "./components/componentsChat/ChatRoom";
 import Profile from "./features/profile/Profile";
 
 function App() {
   return (
     <>
-      <AuthProvider>
-        <AppProvider>
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="home" element={<Home />}>
               <Route index element={<HomePage />} />
-              <Route path="task" element={<Task />} />
-              <Route path="project" element={<Project />} />
               <Route path="chat" element={<Chat />} />
-              <Route path="loginchat" element={<LoginChat />} />
-              <Route path="chatroom" element={<ChatRoom />} />
               <Route path="profile" element={<Profile />} />
             </Route>
           </Routes>
-        </AppProvider>
-      </AuthProvider>
     </>
   );
 }
--- src/features/project/editProject/task/Tasks.tsx
+++
@@ -1,6 +0,0 @@
-import { TabPanel } from '@material-ui/lab'
-import React from 'react'
-
-export const Tasks = () => {
-  return<TabPanel value="2">dsfdfsfd</TabPanel>;
-}
